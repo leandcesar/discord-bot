0b10621819ea862cc3de6420d104da25db566183
Leandro César <ccleandroc@gmail.com>
2024-04-23 18:25:16 -0300

    refactor: move `match` command and `Profile` to `member.py`

--- bot/ext/webhook.py
+++ bot/ext/webhook.py
@@ -11,5 +11,4 @@ async def application_webhook(
     for webhook in webhooks:
         if webhook.application_id == inter.bot.application_id:
             return webhook
-    webhook = await channel.create_webhook(name=inter.bot.user.name)
-    return webhook
+    return await channel.create_webhook(name=inter.bot.user.name)
