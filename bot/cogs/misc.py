4405c46ee2a0a70a7fcbcb46b3101ea2f7b68c76
LuanFabricio <luanlimaleite@hotmail.com>
2024-04-25 17:57:32 -0300

    feat(uptime): Formatting and embedding the uptime command

--- bot/cogs/misc.py
+++ bot/cogs/misc.py
@@ -2,8 +2,16 @@ import disnake
 from disnake.ext import commands
 from disnake.i18n import Localized
 
-from bot.cogs.stalker import ago
 from bot.core import Bot
+
+
+class Uptime(disnake.Embed):
+    def __init__(self, *, description: str, **kwargs) -> None:
+        super().__init__(
+            title="Uptime",
+            description=description,
+            color=disnake.Colour.light_gray(),
+        )
 
 
 class Misc(commands.Cog):
@@ -14,12 +22,11 @@ class Misc(commands.Cog):
         name=Localized(key="COMMAND_UPTIME"), description=Localized("", key="COMMAND_UPTIME_DESC")
     )
     async def uptime(self, inter: disnake.GuildCommandInteraction) -> None:
-        dt = ago(self.bot.init_time)
-        days = dt.days
-        hours, rem = divmod(dt.seconds, 3600)
-        mins, secs = divmod(rem, 60)
-        response = f"{days} days {hours:02d}:{mins:02d}:{secs:02d}"
-        await inter.send(response)
+        locale_name = inter.locale.name.replace("_", "-")
+        message = inter.bot.i18n.get(key="COMMAND_UPTIME_MESSAGE")[locale_name]
+        timestamp = int(self.bot.init_time.timestamp())
+
+        await inter.send(embed=Uptime(description=f"{message} <t:{timestamp}:R>"))
 
 
 def setup(bot: Bot) -> None:
