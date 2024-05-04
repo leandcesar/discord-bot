79f29bb550ddf0449c85ca0ae42aadd72048de09
LuanFabricio <luanlimaleite@hotmail.com>
2024-05-04 10:50:40 -0300

    fix(emote add): Moving text localization to inter.bot.localized

--- bot/cogs/emote.py
+++ bot/cogs/emote.py
@@ -23,8 +23,7 @@ class EmoteEmbed(disnake.Embed):
         image: disnake.File,
         inter: disnake.GuildCommandInteraction,
     ) -> None:
-        locale = inter.locale.name.replace("_", "-")
-        title = inter.bot.i18n.get(key="COMMAND_EMOTE_ADD_TITLE")[locale]
+        title = inter.bot.localized(key="COMMAND_EMOTE_ADD_TITLE", locale=inter.locale)
         title = title.format(name)
 
         super().__init__(
@@ -48,23 +47,22 @@ class EmoteError(disnake.Embed):
         err_enum: EmoteErrorEnum | None = None,
         inter: disnake.GuildCommandInteraction,
     ) -> None:
-        locale = inter.locale.name.replace("_", "-")
         gif_error = None
         description = "IDK"
 
-        title = inter.bot.i18n.get(key="COMMAND_EMOTE_ADD_ERR_TITLE")[locale]
+        title = inter.bot.localized(key="COMMAND_EMOTE_ADD_ERR_TITLE", locale=inter.locale)
         title = title.format(name)
 
         match err_enum:
             case EmoteErrorEnum.WITHOUT_PERMISSION:
                 gif_error = NO_PERM_GIF
-                description = inter.bot.i18n.get(key="COMMAND_EMOTE_ADD_ERR_WITHOUT_PERM")[locale]
+                description = inter.bot.localized(key="COMMAND_EMOTE_ADD_ERR_WITHOUT_PERM", locale=inter.locale)
 
             case EmoteErrorEnum.LARGE_IMAGE:
                 gif_error = LARGE_GIF
-                description = inter.bot.i18n.get(key="COMMAND_EMOTE_ADD_ERR_LARGE_IMAGE")[locale].format(
-                    "https://discord.com/blog/beginners-guide-to-custom-emojis#heading-4"
-                )
+                description = inter.bot.localized(
+                    key="COMMAND_EMOTE_ADD_ERR_LARGE_IMAGE", locale=inter.locale
+                ).format("https://discord.com/blog/beginners-guide-to-custom-emojis#heading-4")
 
             case EmoteErrorEnum.NO_EMOTE_ARGS:
                 gif_error = CHOOSE_GIF
@@ -80,14 +78,7 @@ class EmoteError(disnake.Embed):
 
 
 class Emote(commands.Cog):
-    @commands.slash_command(name=Localized(key="COMMAND_EMOTE"), description=Localized(key="COMMAND_EMOTE_DESC"))
-    async def emote(
-        self,
-        inter: disnake.GuildCommandInteraction,
-    ):
-        pass
-
-    @emote.sub_command(name=Localized(key="COMMAND_EMOTE_ADD"), description=Localized(key="COMMAND_EMOTE_ADD_DESC"))
+    @commands.slash_command(name=Localized(key="COMMAND_EMOTE"), description=Localized(key="COMMAND_EMOTE_ADD_DESC"))
     async def emote_add(
         self,
         inter: disnake.GuildCommandInteraction,
